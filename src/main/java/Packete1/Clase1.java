46d0797c9cdf70bf450a1f5a40bdf2c021a6abd4
evelinqm <maria.quispe.alu@iescampanillas.com>
2021-04-20 10:14:24 +0200

    Updated

---
+++ src/main/java/Packete1/Clase1.java
@@ -0,0 +1,17 @@
+/*
+ * Como en clase1 del packete1 tenemos el modificador de acceso por defecto 
+podemos acceder al mimo desde cualquier otra clase del paquete.sin embrago,
+desde cualquier otro paquete no es posible acceder a no ser que se cambie
+el modificador del paquete a public y se importe el paquete y la clase.
+Private:restinge el modificador para que solo pueda ser usado por métodos de la misma clase 1
+.
+ */
+package Packete1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Clase1 {
+   public  int atributo1;
+}
