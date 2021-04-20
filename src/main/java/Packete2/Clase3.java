96737385cce81fa1fe0df8fc8bd88ef79a3dbe8c
evelinqm <maria.quispe.alu@iescampanillas.com>
2021-04-20 10:21:19 +0200

    Updated

---
+++ src/main/java/Packete2/Clase3.java
@@ -0,0 +1,18 @@
+/*
+ *Modificadores de acceso.
+ */
+package Packete2;
+
+/**
+ *
+ * @author Usuario
+ */
+import Packete1.Clase1;
+
+public class Clase3 {
+    public static void main (String [] args){
+       Clase1 objeto1= new Clase1();
+       objeto1.atributo1=15;
+       
+    }
+}
