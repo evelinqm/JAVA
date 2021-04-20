1df4c8a9bf6478b3e710a9803cd8c5d973035b27
evelinqm <maria.quispe.alu@iescampanillas.com>
2021-04-20 10:38:23 +0200

    Updated

--- src/main/java/Packete1/Clase1.java
+++ src/main/java/Packete1/Clase1.java
@@ -4,7 +4,12 @@ podemos acceder al mimo desde cualquier otra clase del paquete.sin embrago,
 desde cualquier otro paquete no es posible acceder a no ser que se cambie
 el modificador del paquete a public y se importe el paquete y la clase.
 Private:restinge el modificador para que solo pueda ser usado por métodos de la misma clase 1
-.
+.El encapsulamiento: es ocultar algún dato y que solo pueda ser accesible
+a través de los métodos de la misma clase. 
+Setters: Establece un valor para el atributo.Con el elemento setter puedo 
+acceder a atributos encapsulados. 
+
+Getter: Sirve para obtener o mostar
  */
 package Packete1;
 
@@ -13,5 +18,17 @@ package Packete1;
  * @author Usuario
  */
 public class Clase1 {
-   public  int atributo1;
+   private int edad;
+
+   //Estos son los métodos accesores: 
+   
+   //Get sirve para obtener el atributo
+    public int getEdad() {
+        return edad;
+    }
+    //Set sirve para rellenar el atributo
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+   
 }
--- src/main/java/Packete1/Clase2.java
+++ src/main/java/Packete1/Clase2.java
@@ -1,7 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*Como en clase1 del packete1 tenemos el modificador de acceso por defecto 
+podemos acceder al mimo desde cualquier otra clase del paquete.sin embrago,
+desde cualquier otro paquete no es posible acceder a no ser que se cambie
+el modificador del paquete a public y se importe el paquete y la clase.
+Private:restinge el modificador para que solo pueda ser usado por métodos de la misma clase 1
+ *El encapsulamiento: es ocultar algún dato y que solo pueda ser accesible
+a través de los métodos de la misma clase. 
+Setters: Establece un valor para el atributo.Con el elemento setter puedo 
+acceder a atributos encapsulados. 
+
+Getter: Sirve para obtener o mostar
  */
 package Packete1;
 
@@ -12,6 +19,9 @@ package Packete1;
 public class Clase2 {
     public static void main (String [] args){
        Clase1 objeto1= new Clase1();
-       objeto1.atributo1=15;
+       objeto1.setEdad(67);
+       System.out.print("La edad es:"+objeto1.getEdad());
     }
 }
+//Set nos sirve para establecer el valor de la edad de la clase1.
+//Con get nostramos la edad.
